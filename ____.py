da7ba3d3116d74313c3f37cf37d8887a442f7c09
hero24 <hero24@interia.pl>
2020-02-27 22:57:37 +0000

    config: pd fix up proxy file

--- ____.py
+++ ____.py
@@ -1,5 +1,6 @@
 from random import randint
-from pickle import load
-_____, ____, _______, ______ = randint, range, open, load
+from pickle import loads
+_____, ____, _______, ______ = randint, range, open, loads
 ________, __________,  ____________ = ord, chr, eval
 _________, _____________, ___________ = zip, len, globals
+_______________ = bytearray
