90c0d14917974c27352a2b6904fb630ad36d70c3
hero24 <hero24@interia.pl>
2020-02-27 15:25:58 +0000

    Add proxy file
    proxy file for pd

---
+++ ____.py
@@ -0,0 +1,5 @@
+from random import randint
+from pickle import load
+_____, ____, _______, ______ = randint, range, open, load
+________, __________,  ____________ = ord, chr, eval
+_________, _____________, ___________ = zip, len, globals
