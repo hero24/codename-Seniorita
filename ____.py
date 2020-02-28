e2fc97a459db79976f4f2cf0b168a6f9f1c9f5cf
hero24 <hero24@interia.pl>
2020-02-28 15:22:15 +0000

    config/pd: update proxies

--- ____.py
+++ ____.py
@@ -1,6 +1,14 @@
 from random import randint
 from pickle import loads
+from os import listdir, path
+
 _____, ____, _______, ______ = randint, range, open, loads
 ________, __________,  ____________ = ord, chr, eval
 _________, _____________, ___________ = zip, len, globals
 _______________ = bytearray
+
+
+________________ = lambda __: __.isspace()
+_________________ = lambda __: __.sort()
+__________________ = lambda: listdir()
+___________________ = lambda __: path.isfile(__)
