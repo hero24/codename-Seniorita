c5049c99c56067470c839b8d112b6f25bf37017d
hero24 <hero24@interia.pl>
2020-02-28 21:27:53 +0000

    modules: add envbase module to get basic env info like system, platform. etc

---
+++ modules/envbase.py
@@ -0,0 +1,27 @@
+import os
+import platform
+
+def run(**args):
+    s = []
+    system = platform.system()
+    s.append("System Family: %s" %  os.name)
+    s.append("System: %s" % system)
+    s.append("Release: %s" % platform.release())
+    s.append("Platform architecture: %s" % platform.machine())
+    #net_name = platform.node()
+    s.append("Platform: %s" % platform.platform())
+    s.append("CPU: %s" %  platform.processor())
+    if system.lower() == 'windows':
+        rel, ver, csd, ptype = platform.win32_ver()
+        s.append("Windows release: %s" % rel)
+        s.append("Windows version: %s" % ver)
+        s.append("Windows SP: %s" % csd)
+        s.append("Proc type: %s" % ptype)
+        s.append("Windows Edition: %s" % platform.win32_edition())
+        s.append("Is IOT: %s" % platform.win32_is_iot())
+    if system.lower() == 'darwin':
+        s.append(str(platform.mac_ver()))
+    if system.lower() == 'linux':
+        s.append("Libc ver: %s %s" % platform.libc_ver())
+    return "\n".join(s)
+ 
