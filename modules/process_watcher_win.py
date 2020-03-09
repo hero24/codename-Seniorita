94eb1728006b61b0e3265bb75d6cca440513c810
hero24 <hero24@interia.pl>
2020-03-09 17:55:38 +0000

    modules: process watcher add proper io handling
    fix listening duration - passed from config file now
    fix output - return csv file, instead of writing to the disk

--- modules/process_watcher_win.py
+++ modules/process_watcher_win.py
@@ -5,15 +5,15 @@ import win32security
 import wmi
 import sys
 import os
+from time import time
 
-## Todo:
-## - add run(**) function
-## - fix output return
+"""
+    "I'm breaking in, shaping up, then checking out on the prison bus" ~ Imagine Dragons
+"""
 
-def log_to_file(msg):
-    with open("monitor_log.csv", "a") as csv:
-        csv.write("%s\r\n" % msg)
-    return
+def log_to_file(msg, csv=[]):
+    csv.append("%s\r\n" % msg)
+    return csv
 
 
 def get_process_privilages(pid):
@@ -30,30 +30,33 @@ def get_process_privilages(pid):
         priv_list += "N/A"
     return priv_list
 
-log_to_file("Time, User, Executable, CommandLine, PID, Parent PID, Privilages")
 
-c = wmi.WMI()
+def run(**args):
+    if 'runtime' not in args:
+        return
+    mx_tm = int(args['runtime'])
+    csv = log_to_file("Time, User, Executable, CommandLine, PID, Parent PID, Privilages")
+    c = wmi.WMI()
+    process_watcher = c.Win32_Process.watch_for("creation")
+    strt_tm = time()
+    while (time()-strt_tm) <= mx_tm:
+        try:
+            new_process = process_watcher()
+            proc_owner  = new_process.GetOwner()
+            proc_owner  = "%s\\%s" % (proc_owner[0], proc_owner[2])
+            create_date = new_process.CreationDate
+            executable  = new_process.ExecutablePath
+            cmdline     = new_process.CommandLine
+            pid         = new_process.ProcessId
+            parent_pid  = new_process.ParentProcessId
+            privilages  = get_process_privilages(pid)
 
-process_watcher = c.Win32_Process.watch_for("creation")
-
-while True:
-    try:
-        new_process = process_watcher()
-        proc_owner  = new_process.GetOwner()
-        proc_owner  = "%s\\%s" % (proc_owner[0], proc_owner[2])
-        create_date = new_process.CreationDate
-        executable  = new_process.ExecutablePath
-        cmdline     = new_process.CommandLine
-        pid         = new_process.ProcessId
-        parent_pid  = new_process.ParentProcessId
-        privilages  = get_process_privilages(pid)
-        
-        log_msg = "%s, %s, %s, %s, %s, %s, %s" % (
+            log_msg = "%s, %s, %s, %s, %s, %s, %s" % (
                   create_date, proc_owner, executable,
                   cmdline, pid, parent_pid, privilages
                  )
-        print(log_msg)
-        log_to_file(log_msg)
-    except Exception as e:
-        print(e)
-    
+            #print(log_msg)
+            log_to_file(log_msg)
+        except Exception as e:
+            log_to_file(str(e))
+    return "".join(csv)
